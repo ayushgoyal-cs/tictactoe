988a37154a189e6f08a484f8ccbfd1fc5b5dcf9d
ayush <ayush.goyal_cs18@gla.ac.in>
2020-04-07 13:38:41 +0530

    input from user

--- src/tictactoe/Coordinates.java
+++ src/tictactoe/Coordinates.java
@@ -1,11 +1,16 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Coordinates {
 
     void inputCoordinates(char[][] arr) {
+        Scanner sc = new Scanner(System.in);
         boolean inputIsCorrect = false;
         while (!inputIsCorrect) {
-
+            System.out.println("Enter the coordinates: ");
+            int x = sc.nextInt();
+            int y = sc.nextInt();
         }
     }
 }
