9b09e5de712a289adba00a9e4666140ef327c55d
ayush <ayush.goyal_cs18@gla.ac.in>
2020-04-07 15:40:44 +0530

    checking the input is in the limit

--- src/tictactoe/Coordinates.java
+++ src/tictactoe/Coordinates.java
@@ -6,11 +6,16 @@ public class Coordinates {
 
     void inputCoordinates(char[][] arr) {
         Scanner sc = new Scanner(System.in);
+        int noOfO = 0;
+        int noOfX = 0;
         boolean inputIsCorrect = false;
         while (!inputIsCorrect) {
             System.out.println("Enter the coordinates: ");
             int x = sc.nextInt();
             int y = sc.nextInt();
+            if (x < 1 || x > 3 || y < 1 || y > 3) {
+                System.out.println("Coordinates should be from 1 to 3!");
+            }
         }
     }
 }
