506663aacb08ddc0e772f414cee4a02ffdcdc37d
ayush <ayush.goyal_cs18@gla.ac.in>
2020-04-08 15:38:36 +0530

    printing the field in desired format

--- src/tictactoe/print.java
+++ src/tictactoe/print.java
@@ -2,6 +2,14 @@ package tictactoe;
 
 public class print {
         void print(char[][] arr){
-
+                System.out.println("---------");
+                for (char[] x : arr) {
+                        System.out.print("| ");
+                        for (char y : x) {
+                                System.out.print(y + " ");
+                        }
+                        System.out.println("|");
+                }
+                System.out.println("---------");
         }
 }
