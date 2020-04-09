1db2002201c9d6fdea3d6f6a602c5d019fe54137
ayush <ayush.goyal_cs18@gla.ac.in>
2020-04-09 18:42:44 +0530

    condition of winner by row

--- src/tictactoe/Win.java
+++ src/tictactoe/Win.java
@@ -4,5 +4,14 @@ public class Win {
         void win(char[][] arr) {
                 int xWins = 0;
                 int oWins = 0;
+                for (int i = 0; i < 3; i++) {
+                        if (arr[i][0] == arr[i][1] && arr[i][0] == arr[i][2]) {
+                                if (arr[i][0] == 'X') {
+                                        xWins++;
+                                } else if (arr[i][0] == 'O') {
+                                        oWins++;
+                                }
+                        }
+                }
         }
 }
