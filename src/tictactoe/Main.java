284d8c79584b61f7c333e39d7bf962a78494a480
ayush <ayush.goyal_cs18@gla.ac.in>
2020-04-10 12:47:48 +0530

    printing the first fields by Using Print class

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -2,13 +2,13 @@ package tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        int k = 0;
         char[][] arr = new char[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 arr[i][j] = ' ';
             }
         }
+        Print.print(arr);
     }
 
 }
