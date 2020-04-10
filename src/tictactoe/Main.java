a9c14d2b1d84004920b6ad8f6deb6b5ee0c7a8b3
ayush <ayush.goyal_cs18@gla.ac.in>
2020-04-10 12:31:19 +0530

    initializing the 2d array with space

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -2,8 +2,12 @@ package tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        char[][] arr=new char[3][3];
-
+        char[][] arr = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = ' ';
+            }
+        }
     }
 
 }
